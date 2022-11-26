0180d39711f694de56a9581a3f80262282b0a823
devbyanuwat <103254881+devbyanuwat@users.noreply.github.com>
2022-11-26 18:05:19 +0700

    Update db.php

--- database/db.php
+++ database/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "12345678", "weusecar");
+$conn = mysqli_connect("localhost", "root", "", "weusecar");
 mysqli_set_charset($conn, "utf8");
 if(!$conn){
     // echo "failed to connect to DB";
